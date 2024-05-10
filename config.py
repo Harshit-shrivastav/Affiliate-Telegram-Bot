90c40363f3576ed18186329f6b3f37fc195dcd73
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-10 22:19:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-AUTH_USER_ID = int(os.environ.get("AUTH_USER_ID", "5979279455"))
-API_ID = int(os.environ.get("API_ID", "4680197"))
-API_HASH = os.environ.get("API_HASH", "495b0228624028d635bd748b22985f67")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6186291688:AAFVfMLORpFh9O3v9IKrbo1G7ljV6ncLbOM")
+AUTH_USER_ID = int(os.environ.get("AUTH_USER_ID", ""))
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CHAT_ID = int(os.environ.get("CHAT_ID", "-1001415884998")) # id of channel where you want to post deals
 AMAZON_AFFILIATE_TAG = os.environ.get("AMAZON_AFFILIATE_TAG", "")
 FLIPKART_AFFILIATE_TAG = os.environ.get("FLIPKART_AFFILIATE_TAG", "")
-CAPTION = os.environ.get("CAPTION", "**🔥 @DigiDealz**")
+CAPTION = os.environ.get("CAPTION", "") #(optional) markdown supported, caption for below post
 SESSION = os.environ.get("SESSION", "") # telethon session string to forward contents automatically (Optional)
-FROM_CHAT_IDS = list(map(int, getenv("FROM_CHAT_IDS", "-1001315464303 -1001420742409 -1001241952303").split())) # chat Ids from where you want to forward deals 
-TO_CHAT_ID = int(os.environ.get("TO_CHAT_ID", "1538581563")) # your bot id
+FROM_CHAT_IDS = list(map(int, getenv("FROM_CHAT_IDS", "").split())) # chat Ids from where you want to forward deals 
+TO_CHAT_ID = int(os.environ.get("TO_CHAT_ID", "")) # your bot id
